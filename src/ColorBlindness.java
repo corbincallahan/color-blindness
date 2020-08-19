c69fd2101a08cb4620793a355185666859b629da
KevinAlamo <kevalamo@rams.colostate.edu>
2020-08-18 22:08:00 -0600

    started what we talked about

--- src/ColorBlindness.java
+++ src/ColorBlindness.java
@@ -1,4 +1,8 @@
 public class ColorBlindness {
+    // true = works for color blindness
+    private boolean protanopia = true;
+    private boolean deuteranopia = true;
+    private boolean tritanopia = true;
     private int getRed(String hex) {
         String sub = hex.substring(0, 2);
         return Integer.parseInt(sub, 16);
@@ -22,9 +26,40 @@ public class ColorBlindness {
 
         return Math.abs(blue1 - blue2) < 20;
     }
+    // placeholders
+    private boolean idDeuteranopia(String color){
+        return false;
+    }
+
+    private boolean idProtanopia(String color){
+        return false;
+    }
+    private boolean idTritanopia(String color){
+        return false;
+    }
+
+    private void identifyPalette(String palette) {
+        int start = 0;
+        for (int i = 6; i <= palette.length(); i += 7) {
+
+            String color = palette.substring(start, i);
+
+            if (protanopia)
+                protanopia = idProtanopia(color);
+
+            if (deuteranopia)
+                deuteranopia = idDeuteranopia(color);
+
+            if (tritanopia)
+                tritanopia = idTritanopia(color);
+
+            start += i; // need to fix to make sure it doesn't take the " " at the beginning of each color
+        }
+    }
 
     public static void main(String[] args) {
         ColorBlindness inst = new ColorBlindness();
         System.out.println(inst.deuteranomaly("FFFFFF", "FFFFFF"));
+        inst.identifyPalette("0FFFF0 1FFFF1 2FFFF2 3FFFF3");
     }
 }
